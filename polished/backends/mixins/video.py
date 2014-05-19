75d854164cf87289a010f6f350f074350b00890a
Eric Carmichael <eric@ckcollab.com>
2014-05-19 11:37:29 -0700

    adds mpdecimate filter, updates verison

--- polished/backends/mixins/video.py
+++ polished/backends/mixins/video.py
@@ -14,7 +14,7 @@ class VideoMixin(Base):
             "-i", "polished_output/*.png",
             # Scale images to center
             "-vf",
-            'scale=iw*min(1920/iw\,1080/ih):ih*min(1920/iw\,1080/ih),pad=1920:1080:(1920-iw)/2:(1080-ih)/2',
+            'scale=iw*min(1920/iw\,1080/ih):ih*min(1920/iw\,1080/ih),pad=1920:1080:(1920-iw)/2:(1080-ih)/2,mpdecimate',
             # Yes to override
             "-y",
             # Output video
